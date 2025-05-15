8b340a9f83295441421d7e8612103f57fd522ad5
Mohammad Nusairat <mnusairat2003@gmail.com>
2025-05-14 22:21:24 -0500

    Move buildings.json to /app for Railway compatibility

--- backend/src/api.py
+++ backend/src/api.py
@@ -9,9 +9,10 @@ from scipy.spatial import KDTree
 from flask_cors import CORS
 import trie
 import os
+
 print("📂 Current working directory:", os.getcwd())
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-BUILDINGS_FILE = os.path.abspath(os.path.join(BASE_DIR, "..", "..", "data", "buildings.json"))
+
+BUILDINGS_FILE = os.path.abspath("buildings.json")
 print("Looking for buildings file at:", BUILDINGS_FILE)
 
 app = Flask(__name__)
